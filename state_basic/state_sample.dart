ec9eb7a06900b7a022ca8aaae0f9986f2c6190e9
makicamel <unright@gmail.com>
2021-05-17 08:41:00 +0900

    Sample for parent widget manages child's state

---
+++ state_basic/state_sample.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: TapBoxB(
+          active: _active,
+          onChanged: _handleTapBoxChanged,
+        ),
+      );
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() => _active = newValue);
+  }
+}
+
+// 親ウィジェットが子ウィジェットのStateを管理する
+class TapBoxB extends StatelessWidget {
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : assert(active = false),
+        assert(onChanged != null),
+        super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          child: Center(
+            child: Text(
+              active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+        ),
+      );
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+}
